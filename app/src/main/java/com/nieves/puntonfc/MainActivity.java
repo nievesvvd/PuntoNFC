4fe0a4632d2a8a383e2c626a09e7ba9b9b9f21f0
nvelasquez <chibi.pawn@gmail.com>
2016-02-14 20:15:30 +0100

    Editado el README y los comentarios en los metodos

--- app/src/main/java/com/nieves/puntonfc/MainActivity.java
+++ app/src/main/java/com/nieves/puntonfc/MainActivity.java
@@ -1,9 +1,27 @@
+/*
+ *   Copyright (C) 2015, 2016 - Samuel Peregrina Morillas <gaedr@correo.ugr.es>, Nieves V. Velásquez Díaz <nievesvvd@correo.ugr.es>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.nieves.puntonfc;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -13,24 +31,22 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//import android.hardware.Camera.Parameters;
-
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "NfcDemo";
+    public static final String TAG = MainActivity.class.getSimpleName();
     Tag tag;
     private TextView textView;
     private NfcAdapter nfcAdapter;
-    private String key = "hola";
-    Intent intent = getIntent();
-    private CameraManager linterna;
+    private String key = "\u0002eshola";
+
 
     /**
      * Metodo con el que inicializamos la aplicacion y comprobamos tanto si el dispositivo soporta NFC
-     * asi como que este activado o no el mismo
+     * asi como que si esta activado.
      *
      * @param savedInstanceState
      */
@@ -41,37 +57,62 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        mTextView = (TextView) findViewById(R.id.textView_explanation);
-
+        tag = null;
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        textView = (TextView) findViewById(R.id.texto);
+
         //comprobamos si el usuario tiene ncf
         if (nfcAdapter == null) {
-            Toast.makeText(this, "Este dispositivo no es compatible con NFC.", Toast.LENGTH_SHORT).show();
+            textView.setText(R.string.tieneNFC);
             finish();
             return;
         }
         //vemos si esta activado o no el NFC
         if (!nfcAdapter.isEnabled()) {
-            Toast.makeText(this, "NFC esta desactivado.", Toast.LENGTH_SHORT).show();
+            textView.setText(R.string.NFCnoActivo);
+        } else {
+            textView.setText(R.string.NFCactivo);
         }
-        leerTag(intent);
+
+        //creamos un nuevo intent con los datos del tag y lo enviamos a esta actividad
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        // creamos un intetn filtro con los datos tipo MIME
+        IntentFilter tagDetectada = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter ndefIntent = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter[] intentFilters = new IntentFilter[]{tagDetectada, ndefIntent};
     }
 
     /**
-     * Metodo con el que manejaremos que pasa cuando el telefono lee varias tag con la aplicacion
-     * abierta buscando evitar que la abra de nuevo
+     * Metodo que es llamado cuando el usuario aproxima un tag al dispositivo, de forma que evitamos
+     * que se abra la aplicacion de nuevo
      *
-     * @param i
+     * @param intent
      */
-    public void leerTag(Intent i) {
-        Ndef ndef = Ndef.get(tag);
+    @Override
+    protected void onNewIntent(Intent intent) {
+        textView.setText(R.string.FindTag);
+        // if (getIntent().getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
+        tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        setIntent(intent);
+        leerTag(intent);
+        //}
+    }
 
+    /**
+     * Metodo con el que leemos el contenido de una tag NFC
+     *
+     * @param intent
+     */
+    public void leerTag(Intent intent) {
+        Ndef ndef = Ndef.get(tag);
         try {
             ndef.connect();
-            //interface for classes whose instances can be written to and restored from a Parcel.
+
             Parcelable[] mensaje = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            //vemos si el mensaje de la tag esta o no vacio
             if (mensaje != null) {
+
                 NdefMessage[] textPaso = new NdefMessage[mensaje.length];
                 for (int j = 0; j < mensaje.length; j++) {
                     textPaso[j] = (NdefMessage) mensaje[j];
@@ -81,84 +122,76 @@ public class MainActivity extends AppCompatActivity {
                 byte[] payload = record.getPayload();
                 String textoFinal = new String(payload);
 
-                textView.setText("Mensaje leido!! Su contenido es: " + textoFinal);
+                textView.setText(getString(R.string.mensajeLeido) + textoFinal);
                 //una vez leido el texto, veremos si el contenido del mensaje es el que queremos
-                if (textoFinal.toLowerCase() == key) {
-                    encenderLinterna();
-                } else
-                    textView.setText("El contenido del mensaje no es correcto");
+                if (textoFinal.toLowerCase().equals(key)) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+                    startActivity(browserIntent);
+                } else {
+                    Log.d(TAG, "Contenido NFC: " + textoFinal.toLowerCase() + "\nContenido KEY: " + key);
+                    textView.setText(R.string.mensajeFallido);
+                }
 
                 ndef.close();
             }
+
         } catch (Exception ex) {
             Toast.makeText(getApplicationContext(), "No se ha podido leer el mensaje.", Toast.LENGTH_LONG).show();
         }
     }
 
     /**
-     * Metodo que enciende la linterna del dispositivo, para ello vemos si esta o no disponible y
-     * trabajamos en consecuencia a partir de la ID de la camara correcta
+     * Metodo con el que interceptamos el que hacer cuando se detecta una nueva tag de NFC
+     *
+     * @param activity
+     * @param adapter
      */
-    private void encenderLinterna() {
-        String id;
 
-        //si la linterna esta disponible, vemos la camara que la tiene (id) para poder encenderla
-        if (this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
-            id = obtenerIDCamara();
-            linterna.setTorchMode(id, true);
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType("text/plain");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
         }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
     }
 
     /**
-     * Metodo con el que obtenemos el ID de la camara posterior para poder encender la linterna
+     * Metodo con el que finalizamos el interceptar una nueva señal de la tag de NFC
      *
-     * @return
+     * @param activity La peticion correspondiente  para detener foreground dispatch.
+     * @param adapter  The {@link NfcAdapter} used for the foreground dispatch.
      */
-    public String obtenerIDCamara() {
-        final String id;
-
-        for (id:
-             linterna.getCameraIdList()) {
-            CameraCharacteristics caracter = linterna.getCameraCharacteristics(id);
-            int camara = caracter.get(CameraCharacteristics.LENS_FACING);
-            if (camara == CameraCharacteristics.LENS_FACING_BACK)
-                return id;
-        }
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
     }
 
     /**
-     * Metodo que es llamado cuando el usuario aproxima un tag al dispositivo, de forma que evitamos
-     * que se habra la aplicacion de nuevo
-     *
-     * @param intent
+     * Metodo con el que indicamos que hacer cuando vuelva la aplicacion del segundo plano.
      */
-    @Override
-    protected void onNewIntent(Intent intent) {
-        if (getIntent().getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED))
-            tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-
-        setIntent(intent);
-        leerTag(intent);
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
-
-        /**
-         * It's important, that the activity is in the foreground (resumed). Otherwise
-         * an IllegalStateException is thrown.
-         */
-//        setupForegroundDispatch(this, nfcAdapter);
+        setupForegroundDispatch(this, nfcAdapter);
     }
 
+    /**
+     * Metodo con el que le decimos a la aplicacion que hacer cuando se envia a segundo plano.
+     */
     @Override
     protected void onPause() {
-        /**
-         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
-         */
-//        stopForegroundDispatch(this, nfcAdapter);
-
+        stopForegroundDispatch(this, nfcAdapter);
         super.onPause();
     }
 }
